6e0762e697bb974e26ef9e25b8c55e440578745e
OctopusSW <hugo.dlbcq@gmail.com>
2025-07-16 15:30:18 +0200

    ex00

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdelbecq <hdelbecq@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/12 16:32:26 by hdelbecq          #+#    #+#             */
+/*   Updated: 2025/07/16 15:25:26 by hdelbecq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat
+{
+	private:
+		const std::string name;
+		int grade;
+	public:
+		Bureaucrat();
+		Bureaucrat(std::string name);
+		~Bureaucrat();
+		Bureaucrat(const Bureaucrat &other);
+		Bureaucrat &operator=(const Bureaucrat &other);
+		
+		std::string get_Name() const;
+		int get_Grade() const;
+		void set_Grade(int new_grade);
+		void increment_Grade();
+		void decrement_Grade();
+		friend std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				const char* what();
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				const char* what();
+		};
+
+		
+		
+};
+
+
+
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdelbecq <hdelbecq@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/13 16:40:56 by hdelbecq          #+#    #+#             */
+/*   Updated: 2025/07/16 15:29:50 by hdelbecq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int main ()
+{
+	Bureaucrat bureaucrat1("Alice");
+	Bureaucrat bureaucrat2("Bob");
+	Bureaucrat bureaucrat3("Annique");
+	
+	std::cout << bureaucrat1;
+	std::cout << bureaucrat2;
+	std::cout << bureaucrat3;
+
+	try
+	{
+		bureaucrat1.increment_Grade();
+		std::cout << bureaucrat1.get_Name() << " after increment has grade " << bureaucrat1.get_Grade() << std::endl;
+		bureaucrat2.decrement_Grade();
+		std::cout << bureaucrat2.get_Name() << " after decrement has grade " << bureaucrat2.get_Grade() << std::endl;
+	}
+	catch(Bureaucrat::GradeTooHighException &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	catch(Bureaucrat::GradeTooLowException &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	return 0;
+}
